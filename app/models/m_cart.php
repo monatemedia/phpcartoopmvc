8e4a228150c3ea4dc8e93abecf8d5ff186ce83c7
Edward Baitsewe <edward@monatemedia.com>
2022-04-05 01:31:15 +0200

    -- Display products in the cart ---- Build out controller for display

--- app/models/m_cart.php
+++ app/models/m_cart.php
@@ -16,6 +16,42 @@ class cart
     /*
         Getters and Setters
     */
+    /**
+     * Return an array of all product info for items in the cart
+     * 
+     * @access public
+     * @param
+     * @return array, null
+     */
+    public function get()
+    {
+        if  (isset($_SESSION['cart']))
+        {
+            // get all product ids of items in cart_rows
+            $ids = $this->get_ids();
+
+            // use list of ids to get product info from database
+            global $Products;
+            return $Products->get($ids);
+        }
+        return NULL;
+    }
+
+    /**
+     * Return an array of all product ids in cart
+     * 
+     * @access public
+     * @param
+     * @return array, null
+     */
+    public function get_ids()
+    {
+        if (isset($_SESSION['cart']))
+        {
+            return array_keys($_SESSION['cart']);
+        }
+        return NULL;
+    }
 
     /**
      * Adds item to Cart
@@ -56,5 +92,26 @@ class cart
     public function empty_cart()
     {
         unset($_SESSION['cart']);
+    }
+
+    /*
+        Create page parts
+    */
+
+    /**
+     * Return a string, containing list items for each product in cart
+     * 
+     * @access public
+     * @param
+     * @return string
+     */
+    public function create_cart()
+    {
+        // get products currently in cart
+        $products = $this->get();
+
+        echo "<pre>";
+        print_r(($products));
+        echo "</pre>";
     }
 }
